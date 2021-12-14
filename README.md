ef3f50634bf7e795ea01cb81c6e9e96598564e24
Andrej <43913051+andrej-dyck@users.noreply.github.com>
2021-12-14 23:52:04 +0100

    Setup kotlin gradle project

---
+++ README.md
@@ -0,0 +1,21 @@
+# tdd-workshop
+
+![build](https://github.com/andrej-dyck/tdd-workshop/actions/workflows/gradle-ci.yml/badge.svg?branch=main)
+
+## Build with Gradle
+```
+./gradlew build
+```
+
+#### Only Assemble
+```
+./gradlew assemble
+```
+#### Only Detekt
+```
+./gradlew detekt
+```
+#### Only Tests
+```
+./gradlew test
+```
---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+    id("io.gitlab.arturbosch.detekt").version("1.19.0")
+}
+
+group = "andrej.dyck.tdd.workshop"
+version = "1.0"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    // JUnit 5
+    testImplementation(platform("org.junit:junit-bom:5.8.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    // AssertJ
+    testImplementation("org.assertj:assertj-core:3.21.0")
+}
+
+/* Source sets by Kotlin conventions /src and /test */
+val sources = setOf("main" to "src/", "test" to "test/")
+kotlin {
+    sources.forEach { (set, dir) ->
+        sourceSets[set].apply { kotlin.srcDirs(dir) }
+    }
+}
+
+/* Resources */
+sourceSets["main"].resources.srcDirs("resources")
+sourceSets["test"].resources.srcDirs("test-resources")
+
+/* Detekt */
+detekt {
+    source = files(sources.map { it.second })
+    config = files("detekt.yml")
+}
+
+/* Check with JUnit 5 */
+tasks.test {
+    useJUnitPlatform {
+        includeEngines("junit-jupiter")
+        excludeEngines("junit-vintage")
+    }
+}
+
+/* Gradle wrapper */
+tasks.withType<Wrapper> {
+    gradleVersion = "7.3"
+    distributionType = Wrapper.DistributionType.BIN
+}
